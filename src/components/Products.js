abd2172f726e5ffe294e0995d77915b4b69bfd29
Sammy3000 <towettsammy30@gmail.com>
2024-05-02 09:33:13 +0300

    Modify width and height

--- src/components/Products.js
+++ src/components/Products.js
@@ -16,7 +16,7 @@ useEffect(()=>{
   const cards = product.map(prod=> (
     <div className='col-md-3'>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={prod.image}/>
+      <Card.Img variant="top" src={prod.image} style = {{width:'100px' , height: '125px'}}/>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
