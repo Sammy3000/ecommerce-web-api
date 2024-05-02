402338da0b62863686f063675620ff31d3cb9c75
Sammy3000 <towettsammy30@gmail.com>
2024-05-02 09:34:43 +0300

    Center my images

--- src/components/Products.js
+++ src/components/Products.js
@@ -16,7 +16,9 @@ useEffect(()=>{
   const cards = product.map(prod=> (
     <div className='col-md-3'>
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={prod.image} style = {{width:'100px' , height: '125px'}}/>
+        <div className='text-center'>
+          <Card.Img variant="top" src={prod.image} style = {{width:'100px' , height: '125px'}}/>
+        </div>
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
         <Card.Text>
