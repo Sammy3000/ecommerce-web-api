b440e9a7f4514ca211684baf267546b454fa9253
Sammy3000 <towettsammy30@gmail.com>
2024-05-02 09:37:18 +0300

    Use product description from api

--- src/components/Products.js
+++ src/components/Products.js
@@ -22,8 +22,7 @@ useEffect(()=>{
       <Card.Body>
         <Card.Title>{prod.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {prod.description}
         </Card.Text>
         <Button variant="primary">Go somewhere</Button>
       </Card.Body>
