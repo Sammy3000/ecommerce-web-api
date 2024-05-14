a76fae7a87c7d0b6b3563204d5a13541f7eb73b8
Sammy3000 <towettsammy30@gmail.com>
2024-05-14 15:38:16 +0300

    Modify component

--- src/components/NavbarPanel.js
+++ src/components/NavbarPanel.js
@@ -11,8 +11,6 @@ const NavbarPanel = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container fluid>
       <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
-      <Navbar.Toggle aria-controls="navbarScroll" />
-      <Navbar.Collapse id="navbarScroll">
         <Nav
           className="me-auto my-2 my-lg-0"
           style={{ maxHeight: '100px' }}
