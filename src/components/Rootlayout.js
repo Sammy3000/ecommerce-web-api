0df07a5cd6c1b95c332cb5caa1bb9e7edc26d056
Sammy3000 <towettsammy30@gmail.com>
2024-05-14 15:11:28 +0300

    Modify root layout

--- src/components/Rootlayout.js
+++ src/components/Rootlayout.js
@@ -1,9 +1,12 @@
  import { Outlet } from "react-router-dom"
  
  const Rootlayout = ()=>{
-    <>
-     <h2>Navigation</h2>
-    </>
+   return(
+      <>
+      <h2>Navigation</h2>
+     </>
+   )
+   
  }
 
  export default Rootlayout
