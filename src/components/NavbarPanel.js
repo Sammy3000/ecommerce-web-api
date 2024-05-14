81da7c6ed6b9948eb24bd46796a3f0811cec7c88
Sammy3000 <towettsammy30@gmail.com>
2024-05-14 15:41:50 +0300

    Modify component

--- src/components/NavbarPanel.js
+++ src/components/NavbarPanel.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavbarCollapse } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -9,19 +8,13 @@ import { Link } from 'react-router-dom';
 const NavbarPanel = () => {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
-    <Container fluid>
+    < Container fluid>
       <Navbar.Brand href="#">Redux Toolkit</Navbar.Brand>
-        <Nav
-          className="me-auto my-2 my-lg-0"
-          style={{ maxHeight: '100px' }}
-          navbarScroll
-        >
+       <Nav>
           <Nav.Link to='/' as={Link} >Products</Nav.Link>
         </Nav>
         <Navbar.Toggle/>
         <Navbar.Collapse></Navbar.Collapse>
-      
-      </Navbar.Collapse>
     </Container>
   </Navbar>
   )
