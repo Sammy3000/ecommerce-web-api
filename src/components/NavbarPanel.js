6c5da6a9b6627162cfc1cb0df42ea93d28e19fa5
Sammy3000 <towettsammy30@gmail.com>
2024-05-14 15:28:39 +0300

    Delete link

--- src/components/NavbarPanel.js
+++ src/components/NavbarPanel.js
@@ -19,7 +19,6 @@ const NavbarPanel = () => {
           navbarScroll
         >
           <Nav.Link href="#action1">Product</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
           <NavDropdown title="Link" id="navbarScrollingDropdown">
             <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action4">
