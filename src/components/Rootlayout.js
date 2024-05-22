847b38f266b177d278b4ac236f375d3bd0b79947
Sammy3000 <towettsammy30@gmail.com>
2024-05-22 18:03:41 +0300

    Import Store

--- src/components/Rootlayout.js
+++ src/components/Rootlayout.js
@@ -1,6 +1,7 @@
  import { Outlet } from "react-router-dom"
 import NavbarPanel from "./NavbarPanel"
 import { Provider } from "react-redux"
+import Store from "../store/Store"
  
  const Rootlayout = ()=>{
    return(
