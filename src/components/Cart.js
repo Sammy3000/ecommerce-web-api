bc34ad78ff269f74ab46da916b26426e1da2b152
Sammy3000 <towettsammy30@gmail.com>
2024-05-22 17:27:42 +0300

    Comment out code

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,18 @@
 import { useState } from "react"
 
 const Cart = ()=>{
-    const [count,setCount] = useState(0)
-    const handleCount = ()=>{
-        setCount(count + 1)
-    }
+    // const [count,setCount] = useState(0)
+    // const handleCount = ()=>{
+    //     setCount(count + 1)
+    // }
 
     return(
         <>
-        {/* <h2>Cart</h2> */}
-        <div>
+        <h2>Cart</h2>
+        {/* <div>
             <button onClick={handleCount}>You have clicked {count} times</button>
         </div>
-     
+      */}
         </>
     )
 }
