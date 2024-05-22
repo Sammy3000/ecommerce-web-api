7a7661b4bcbede3bec386c44dd20bf866a46726c
Sammy3000 <towettsammy30@gmail.com>
2024-05-22 17:45:00 +0300

    Modify cart slice

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -3,5 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [];
 
 const cartSlice = createSlice({
+    name:'cart',
+    initialState,
+    reducers:{
+        
+    }
 
 })
