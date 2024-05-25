fc4cac15cd6141d19b09bbee9ab5e0531294c60c
Sammy3000 <towettsammy30@gmail.com>
2024-05-25 11:40:36 +0300

    Modify store

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./cartSlice";
+import dummySlice from "./dummySlice";
 
 const Store = configureStore({
     reducer:{
         cart: cartReducer,
+        add: dummySlice
 
     }
 })
