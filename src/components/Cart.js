53c2fdc59d88485deb96798a797a349571ad0714
Sammy3000 <towettsammy30@gmail.com>
2024-05-30 09:19:19 +0300

    Modify cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,18 +1,9 @@
-// import { useState } from "react"
 
 const Cart = ()=>{
-    // const [count,setCount] = useState(0)
-    // const handleCount = ()=>{
-    //     setCount(count + 1)
-    // }
 
     return(
         <>
         <h2>Cart</h2>
-        {/* <div>
-            <button onClick={handleCount}>You have clicked {count} times</button>
-        </div>
-      */}
         </>
     )
 }
