a89d83297aac6bdbc42a70873b3e226309ce5f64
Sammy3000 <towettsammy30@gmail.com>
2024-05-30 09:34:44 +0300

    Modify cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,36 @@
 import {useSelector} from 'react-redux'
+import { Card } from 'react-bootstrap'
+import {Button} from 'react-bootstrap'
+
 const Cart = ()=>{
-    const cartProduct = useSelector(store=>store.cart)
+    const products = useSelector(store=>store.cart)
+
+    const cards = products.map(prod=> (
+        <div className='col-md-3'>
+          <Card key={prod.id} className='h-100'>
+            <div className='text-center'>
+              <Card.Img variant="top" src={prod.image} style = {{width:'100px' , height: '125px'}}/>
+            </div>
+          <Card.Body>
+            <Card.Title>{prod.title}</Card.Title>
+            <Card.Text>
+              $: {prod.price}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+           <Button variant="primary">Add to Cart</Button>
+          </Card.Footer>
+        </Card>
+        </div>
+    
+      ))
 
     return(
         <>
-        <h2>Cart</h2>
+      <div className='row'>
+        {cards}
+
+      </div>
         </>
     )
 }
